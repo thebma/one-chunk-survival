a1a016e4b110d5363cf52df49289cf6c19ee9409
thebma <boum.dev@protonmail.com>
2022-04-16 18:58:55 +0200

    touch the config file, register commands in plugin.yml

--- src/main/java/nl/midnan/onechunksurvival/OneChunkSurvival.java
+++ src/main/java/nl/midnan/onechunksurvival/OneChunkSurvival.java
@@ -6,7 +6,9 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.logging.Level;
 
 public class OneChunkSurvival extends JavaPlugin {
@@ -15,6 +17,7 @@ public class OneChunkSurvival extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        loadOrCreateConfig();
         getLogger().info("One-chunk Survival Plugin Enabled!");
         getCommand("ocs-start").setExecutor(new OneChunkSurvivalStartCommandExecutor(this));
         getCommand("ocs-stop").setExecutor(new OneChunkSurvivalStopCommandExecutor(this));
@@ -27,11 +30,21 @@ public class OneChunkSurvival extends JavaPlugin {
     private void loadOrCreateConfig()
     {
         configurationFile = new File(getDataFolder(), "ocs.yml");
+        getLogger().info("Config at " + getDataFolder());
 
         if(!configurationFile.exists()) {
+            getLogger().info("Wrote config!");
             configurationFile.getParentFile().mkdirs();
-            saveResource("ocs.yml", false);
+
+            //Touch file...
+            try {
+                new FileOutputStream(configurationFile).close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
+        getLogger().info("Config at " + getDataFolder());
 
         configuration = new YamlConfiguration();
         try {
@@ -40,7 +53,6 @@ public class OneChunkSurvival extends JavaPlugin {
             getLogger().log(Level.ALL, "Could not load config...");
         }
     }
-
 
     @Override
     public void onDisable() {
